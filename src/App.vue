b390a4df9378a5c26c2a4f72c82653c78cb2b3d3
박수현 <sh@immerse.co.kr>
2024-05-16 00:17:15 +0900

    sticky 헤더 추가, Options API로 수정

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,66 @@
 <template>
-  <Header />
+  <Header
+    :class="{ 'header--sticky': isSticky, 'sticky--light': isLightHeader }"
+    @scrollTo="scrollToSection"
+  />
   <main>
-    <HeroSection @scrollToAbout="scrollToAbout" />
-    <AboutSection ref="aboutRefEl" />
-    <SkillSection />
-    <WorksSection />
+    <div ref="heroSectionRef">
+      <HeroSection @scrollToAbout="scrollToSection(aboutSectionRef)" />
+    </div>
+    <div ref="aboutSectionRef">
+      <AboutSection />
+    </div>
+    <div ref="skillSectionRef">
+      <SkillSection />
+    </div>
+    <div ref="worksSectionRef">
+      <WorksSection />
+    </div>
   </main>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script>
 import Header from '@/components/layouts/HeaderLayout.vue';
 import HeroSection from '@/components/sections/HeroSection.vue';
 import AboutSection from '@/components/sections/AboutSection.vue';
 import SkillSection from '@/components/sections/SkillSection.vue';
 import WorksSection from '@/components/sections/WorksSection.vue';
 
-const aboutRefEl = ref(null);
+export default {
+  components: {
+    Header,
+    HeroSection,
+    AboutSection,
+    SkillSection,
+    WorksSection,
+  },
+  data() {
+    return {
+      isSticky: false,
+      isLightHeader: false,
+    };
+  },
+  methods: {
+    scrollToSection(sectionRef) {
+      const element = this.$refs[sectionRef];
+      if (element) element.scrollIntoView({ behavior: 'smooth' });
+    },
+    handleScroll() {
+      const stickyPoint = this.$refs.heroSectionRef.offsetHeight;
+      const lightHeaderPoint = stickyPoint + this.$refs.aboutSectionRef.offsetHeight;
+      const scrollTop = window.scrollY;
 
-const scrollToAbout = () => {
-  aboutRefEl.value?.scrollToSection();
+      console.log(lightHeaderPoint);
+      this.isSticky = scrollTop >= stickyPoint;
+      this.isLightHeader = scrollTop >= lightHeaderPoint;
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
 };
 </script>
 
