1e6f024fd2cc217d24f40cfd6bd02a1bd077222a
sootopia <shpark7502@naver.com>
2024-05-19 00:37:13 +0900

    커스텀 Textarea 컴포넌트 추가

---
+++ src/components/MyTextarea.vue
@@ -0,0 +1,15 @@
+<template>
+  <textarea
+    class="block w-full px-4 py-3 font-medium text-gray-600 placeholder:text-gray-300 border border-gray-200 rounded-xl resize-none bg-white outline-none focus:border-teal-600 focus:shadow-[0px_0px_0px_2px_rgba(13,148,136,0.2)]"
+    @input="$emit('update:modelValue', $event.target.value)"
+    >{{ modelValue }}</textarea
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: String,
+  },
+};
+</script>
