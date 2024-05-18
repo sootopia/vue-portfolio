2ef548897d9fd973bb2c2afac6e4af9e37fa08f0
sootopia <shpark7502@naver.com>
2024-05-19 00:36:42 +0900

    푸터 추가

---
+++ src/components/layouts/FooterLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="text-center py-16 bg-gray-100">
+    <div class="container mx-auto px-6">
+      <p class="text-sm font-medium text-slate-400">&copy; 2024 Park Soohyun</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
