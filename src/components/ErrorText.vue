93360e0ccf731f2f1373a9be51008993969a55b3
sootopia <shpark7502@naver.com>
2024-05-19 00:37:30 +0900

    오류 텍스트 컴포넌트 추가

---
+++ src/components/ErrorText.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-sm font-semibold text-red-500 mt-2">{{ message }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
