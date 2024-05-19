fbfccc09838a9bdfafb72e171b20187410a19fa1
sootopia <shpark7502@naver.com>
2024-05-19 21:58:40 +0900

    문의 모달내 정보 리스트 컴포넌트 생성

---
+++ src/components/ContactList.vue
@@ -0,0 +1,8 @@
+<template>
+  <li class="relative pl-7 text-slate-500 leading-7">
+    <div class="icon absolute top-[6px] left-0 text-slate-400">
+      <slot name="icon"></slot>
+    </div>
+    <slot name="content"></slot>
+  </li>
+</template>
