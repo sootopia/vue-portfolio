7d3ecaf9bab5f9a7f856c185f5ae89244455a2e2
sootopia <shpark7502@naver.com>
2024-05-19 18:22:34 +0900

    props 추가, z-index 수정

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed inset-0 z-[900] overflow-x-hidden overflow-y-auto bg-[#333333]/75"
+    class="fixed inset-0 z-[1150] overflow-x-hidden overflow-y-auto bg-[#333333]/75"
     tabindex="-1"
     aria-modal="true"
     role="dialog"
@@ -10,7 +10,7 @@
       class="flex flex-col max-w-[600px] xl:max-w-[500px] h-full mx-6 sm:mx-auto py-8 justify-center items-center"
     >
       <div
-        class="relative flex flex-col w-full p-8 rounded-3xl bg-white shadow-[0px_8px_32px_8px_rgba(33,33,33,0.08)]"
+        class="relative flex flex-col w-full p-8 rounded-3xl bg-white overflow-hidden shadow-[0px_8px_32px_8px_rgba(33,33,33,0.08)]"
         @click.stop
       >
         <div class="flex flex-wrap items-center justify-between">
@@ -41,9 +41,12 @@
 
 <script>
 export default {
+  props: {
+    disableDismiss: Boolean,
+  },
   methods: {
     handleCloseModal() {
-      this.$emit('close');
+      if (!this.disableDismiss) this.$emit('close');
     },
   },
 };
